b1835b4e9f4351b78efc84b2d2c509086d638b6c
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-11-11 22:06:44 +0100

    added timer with start/stop and reset actions and effects

--- TCA-todo/Features/TaskFeature.swift
+++ TCA-todo/Features/TaskFeature.swift
@@ -14,13 +14,20 @@ struct TaskFeature {
         var isCompleted = false
         var name: String?
         var isLoading = false
+        var count = 0
+        var isTimerRunning = false
     }
     
     enum Action {
         case toggleCompleted
         case randomTitleButtonTapped
         case randomTitleResponse(String)
+        case timerTick
+        case timerStopped
+        case toggleTimerButtonTapped
     }
+    
+    enum CancelID { case timer }
     
     
     var body: some ReducerOf<Self> {
@@ -44,6 +51,29 @@ struct TaskFeature {
                 state.name = name
                 state.isLoading = false
                 return .none
+            case .timerTick:
+                state.count += 1
+                return .none
+            case .timerStopped:
+                state.count = 0
+                state.isTimerRunning = false
+                return .none
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer).merge(
+                        with: .run { send in
+                            await send(.timerStopped)
+                    })
+                }
             }
         }
     }
--- TCA-todo/Views/TaskCellView.swift
+++ TCA-todo/Views/TaskCellView.swift
@@ -12,40 +12,60 @@ struct TaskCellView: View {
     let store: StoreOf<TaskFeature>
     
     var body: some View {
-        HStack {
-            Button {
-                store.send(.toggleCompleted)
-            } label: {
-                HStack {
-                    Image(systemName: store.isCompleted ? "checkmark.square.fill" : "square")
-                        .font(.title3.weight(.medium))
-                        .symbolRenderingMode(.hierarchical)
-                        .foregroundStyle(store.isCompleted ? Color.accentColor : .secondary)
-                    
-                    Text(store.name ?? "Shuffle todo...")
-                        .font(.headline)
-                        .strikethrough(store.isCompleted)
-                        .foregroundStyle(store.isCompleted ? .secondary : .primary)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                .background(.background.opacity(0.01))
-            }
-            .buttonStyle(.plain)
-
-            
-            if store.isLoading {
-                ProgressView()
-            } else {
+        VStack(alignment: .trailing, spacing: 0) {
+            HStack {
                 Button {
-                    store.send(.randomTitleButtonTapped)
+                    store.send(.toggleCompleted)
                 } label: {
-                    Image(systemName: "shuffle")
-                        .foregroundStyle(.secondary)
+                    HStack {
+                        Image(systemName: store.isCompleted ? "checkmark.square.fill" : "square")
+                            .font(.title3.weight(.medium))
+                            .symbolRenderingMode(.hierarchical)
+                            .foregroundStyle(store.isCompleted ? Color.accentColor : .secondary)
+                        
+                        Text(store.name ?? "Shuffle todo...")
+                            .font(.headline)
+                            .strikethrough(store.isCompleted)
+                            .foregroundStyle(store.isCompleted ? .secondary : .primary)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .background(.background.opacity(0.01))
                 }
-                .disabled(store.isLoading)
+                .buttonStyle(.plain)
+                
+                
+                if store.isLoading {
+                    ProgressView()
+                } else {
+                    Button {
+                        store.send(.randomTitleButtonTapped)
+                    } label: {
+                        Image(systemName: "shuffle")
+                            .foregroundStyle(.secondary)
+                    }
+                    .disabled(store.isLoading)
+                }
+            }
+            .padding()
+            .background(.thinMaterial, in: .rect(cornerRadius: 9))
+            
+            HStack {
+                if store.isTimerRunning {
+                    Text(store.count.formatted())
+                        .font(.headline)
+                        .contentTransition(.numericText())
+                        .padding(.horizontal)
+                }
+                
+                Spacer()
+                
+                Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+                    store.send(.toggleTimerButtonTapped)
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(5)
             }
         }
-        .padding()
         .background(.regularMaterial, in: .rect(cornerRadius: 9))
     }
 }
