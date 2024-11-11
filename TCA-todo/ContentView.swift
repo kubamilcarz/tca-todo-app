7573eee9c588b2ed9786eaf9cff87c4b0483a0bf
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-11-11 21:06:31 +0100

    Initial Commit

---
+++ TCA-todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA-todo
+//
+//  Created by Kuba on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA-todo/TCA_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_todoApp.swift
+//  TCA-todo
+//
+//  Created by Kuba on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
